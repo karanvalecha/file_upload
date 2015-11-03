a6bf1a58095552e73f17c31ede7eda60f0fa117f
Karan Valecha <karan.valecha@kiprosh.com>
2015-11-03 12:45:11 +0530

    Created Upload model and a public folder

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,5 +1,22 @@
 class UploadController < ApplicationController
   def index
-    
+  end
+
+  def create
+    content = params[:content].read
+    name = params[:content].original_filename
+
+    uploaded_file = Upload.create!(content: content) 
+
+    # Open a new stream and write to the file.
+    File.open("#{Rails.root}/public/uploads/#{uploaded_file.id}#{name}", "w") do |file| 
+      begin
+        file.write(content)
+      ensure
+        file.close
+      end
+    end
+    flash[:success] = "The file was uploaded succesfully."
+    redirect_to root_url
   end
 end
