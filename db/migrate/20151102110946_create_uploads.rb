750eb65f7b2f5837c89946c4ce91f6724025096a
Karan Valecha <karan.valecha@kiprosh.com>
2015-11-04 20:28:11 +0530

    Removed useless models

--- db/migrate/20151102110946_create_uploads.rb
+++
@@ -1,9 +0,0 @@
-class CreateUploads < ActiveRecord::Migration
-  def change
-    create_table :uploads do |t|
-      t.string :content
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102110946) do
-
-  create_table "uploads", force: :cascade do |t|
-    t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
