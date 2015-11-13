213b375b42908252ab963de4d041315076fe03fe
Karan Valecha <karan.valecha@kiprosh.com>
2015-11-13 19:21:19 +0530

    Modified for validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: { minimum: 3, maximum: 50 }
 	validates :email, presence: true, length: {	minimum: 6, maximum: 255 }
 
+	validates_uniqueness_of :email, case_sensitive: false
+
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
 	after_create :create_folder
