f7df4018224ca7044c726ff7f1a64adf766b226f
Karan Valecha <karan.valecha@kiprosh.com>
2015-11-19 13:36:32 +0530

    Using the 'let'

--- spec/controllers/concerns_spec.rb
+++ spec/controllers/concerns_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 RSpec.describe UploadsController, type: :controller do
   include UtilityFileMethods
   include FileMethods
-
   before :all do
-    @user = create(:user)
-    @upload = create(:upload, user: @user)
-    @file_path = @upload.file_path
+    user = create(:user)
+    upload = create(:upload, user: user)
+    @file_path = upload.file_path
     @content = File.read("#{Rails.root.join}/spec/fixtures/sample.txt")
     FileUtils.cp("#{Rails.root.join}/spec/fixtures/sample.txt", @file_path)
   end
 
   describe "File Utility Method" do
+    let(:file) {File.read(@file_path)}
 
     context 'Insert "Hii" at line 1' do
       it "should work" do
         cmd = "i 1 'Hii'"
         update_actions cmd
-        expect(File.read(@file_path)).to eq "Hii\n#{@content}"
+        expect(file).to eq "Hii\n#{@content}"
       end
     end
 
@@ -26,7 +26,7 @@ RSpec.describe UploadsController, type: :controller do
       it "should work" do
         cmd = "rev 1"
         update_actions cmd
-        expect(File.read(@file_path)).to eq "iiH\n#{@content}"
+        expect(file).to eq "iiH\n#{@content}"
       end
     end
 
@@ -34,7 +34,7 @@ RSpec.describe UploadsController, type: :controller do
       it "should work" do
         cmd = "r 'iiH' 'Hii'"
         update_actions cmd
-        expect(File.read(@file_path)).to eq "Hii\n#{@content}"
+        expect(file).to eq "Hii\n#{@content}"
       end
     end
 
@@ -42,7 +42,7 @@ RSpec.describe UploadsController, type: :controller do
       it "should work" do
         cmd = "del 1"
         update_actions cmd
-        expect(File.read(@file_path)).to eq @content
+        expect(file).to eq @content
       end
     end
 
